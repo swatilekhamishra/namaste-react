7b277ba2f029a7f2a52f10ebf3d06c316901ef71
swatilekhamishra <123244682+swatilekhamishra@users.noreply.github.com>
2023-02-05 06:48:07 +0530

    Igniting React App-Using React Library

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading= React.createElement ("h1",{id:"title"},"Heading 1");
 
 const heading2=React.createElement("h2",{id:"title"},"Heading 2");
